b90b1eb3342863d32f330968d6346a2fa91d5931
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-10-27 21:11:36 +0100

    still testing rendered a button

--- index.js
+++ index.js
@@ -28,11 +28,34 @@ const sendMessage = async (chatId, text) => {
         await axios.post(`${TELEGRAM_API}/sendMessage`, {
             chat_id: chatId,
             text: text,
+          reply_markup: reply_markup = {
+                
+               keyboard: [ 
+                ["send a poll", "pin a chat"],
+                ["echo messages","say what i say"]
+            ],
+            resize_keyboard:true 
+          },
+          
+
         });
     } catch (error) {
         console.error('Error sending message:', error);
     }
 };
+
+const createPoll = async(chatId,question,options)=> {
+
+    try {
+        await axios.post(`${TELEGRAM_API}/sendPoll`, {
+            chat_id:chatId,
+            question:question,
+            options:options
+        })
+    }catch(error) {
+        console.log(error)
+    }
+}
 
 
 
@@ -51,11 +74,36 @@ const startPolling = async () => {
                 // Echo the received message back
                 sendMessage(chatId, text);
 
+                createPoll(chatId,"your age",[
+                    {
+                        text:"15"
+                    },{
+                        text:"18"
+                    },
+                    {
+                        text:"23"
+                    }
+                ])
+
                 // Update offset for the next request
                 offset = update.update_id + 1;
                 console.log("offset is : "+ offset)
                 console.log("chat id : "+ chatId)
-                }
+                }/*
+                else if(update.message.chat){
+                    const chatId = update.message.chat.id
+
+                    createPoll(chatId,"your age",[
+                        {
+                            text:"15"
+                        },{
+                            text:"18"
+                        },
+                        {
+                            text:"23"
+                        }
+                    ])
+                }*/
             });
         }
 
