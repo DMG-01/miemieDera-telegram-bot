c8930eabe680a18857da0b194f3921019838ba57
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-11-05 01:24:58 +0100

    user model done

--- models/usersModel.js
+++ models/usersModel.js
@@ -3,6 +3,21 @@ const mongoose = require("mongoose")
 const userModel = new mongoose.Schema({
     userId: {
         type:Number,
-        required: [true, "please enter "]
+        required: [true, "user id is required"]
+    },
+    firstName: {
+        type:String,
+        required:[true,"firstName is required"]
+    },
+    lastName: {
+        type:String
+    },
+    userName: {
+        type:String,
+        required:[true,"userName is required"]
     }
-})
+    
+},{timestamps:true})
+
+module.exports = mongoose.model("user",userModel)
+
